e8a6eedc6c9168d88615ce9ab692193c9d4f624c
Mo Russ <mauriceruss302@gmail.com>
2020-02-19 07:25:13 -0500

    testGetId done 2192020

--- src/test/java/rocks/zipcodewilmington/CatTest.java
+++ src/test/java/rocks/zipcodewilmington/CatTest.java
@@ -44,11 +44,11 @@ public class CatTest {
     @Test
     public void testSetName(){
         //given
-        Cat cat = new Cat("Zula",null,null);
+        Cat cat1 = new Cat("Zula",null,null);
         String givenName = "Zula";
         //when
-        cat.setName(givenName);
-        String catName = cat.getName();
+        cat1.setName(givenName);
+        String catName = cat1.getName();
         //then
         Assert.assertEquals(catName, givenName);
     }
@@ -57,11 +57,11 @@ public class CatTest {
     @Test
     public void testSpeak(){
         //given
-        Cat cat = new Cat(null,null,null);
+        Cat cat1 = new Cat(null,null,null);
         String expected = "meow!";
 
         //when
-        String actual = cat.speak();
+        String actual = cat1.speak();
 
         //then
         Assert.assertEquals(expected,actual);
@@ -71,14 +71,14 @@ public class CatTest {
     @Test
     public void testSetBirthDate(){
         //given
-        Cat cat = new Cat(null, null, null);
+        Cat cat1 = new Cat(null, null, null);
         Date birthDate = new Date();
         //when
-        cat.setBirthDate(birthDate);
+        cat1.setBirthDate(birthDate);
         Date expected = birthDate;
 
         //then
-        Date actual = cat.getBirthDate();
+        Date actual = cat1.getBirthDate();
         Assert.assertEquals(expected, actual);
     }
 
@@ -86,16 +86,29 @@ public class CatTest {
     @Test
     public void testEat(){
         //given
-        Cat cat = new Cat(null, null, null);
+        Cat cat1 = new Cat(null, null, null);
         Food can = new Food();
         //when
-        cat.eat(can);
+        cat1.eat(can);
         Integer expected = 1;
         //then
-        Integer actual = cat.getNumberOfMealsEaten();
+        Integer actual = cat1.getNumberOfMealsEaten();
+        Assert.assertEquals(expected,actual);
+    }
+
+    // TODO - Create tests for `Integer getId()`
+    @Test
+    public void testGetId(){
+        //given
+        Cat cat1 = new Cat(null,null,1);
+        //when
+        Integer expected = 1;
+        //then
+        Integer actual = cat1.getId();
         Assert.assertEquals(expected,actual);
 
+        }
 
 
     }
-}
+
